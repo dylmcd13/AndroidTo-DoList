12e59dca8a5e35f29d63fbdd9ebaf916116547fa
dylmcd13 <dylmcd13@gmail.com>
2022-11-26 18:58:40 -0500

    Changed text size when using add button

--- app/src/main/java/com/example/to_dolist/MainActivity.java
+++ app/src/main/java/com/example/to_dolist/MainActivity.java
@@ -10,6 +10,7 @@ import android.content.res.Configuration;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -33,13 +34,29 @@ public class MainActivity extends AppCompatActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
     LinearLayout list = findViewById(R.id.addingTest);
+    LinearLayout.LayoutParams listParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 5);
 
     addBtn = findViewById(R.id.addTaskBtn);
     addBtn.setOnClickListener((View v) -> {
+//      View dividerTop = new View(this);
+//      dividerTop.setBackgroundColor(getResources().getColor(R.color.white));
+//      list.addView(dividerTop,listParams);
+
+
+
+
       TextView test = new TextView(this);
       test.setTextColor(getResources().getColor(R.color.white));
-      test.setText("Hello@@!");
+      test.setTextSize(30);
+      test.setText("TextView");
       list.addView(test);
+
+
+      View dividerBottom = new View(list.getContext());
+      dividerBottom.setBackgroundColor(getResources().getColor(R.color.white));
+      dividerBottom.setTextAlignment(View.TEXT_ALIGNMENT_INHERIT);
+
+      list.addView(dividerBottom,listParams);
     });
 
   }
