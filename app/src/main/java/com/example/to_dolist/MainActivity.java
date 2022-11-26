5c1d6b861b15fa835df8e09a2d2b2ece44e9191d
dylmcd13 <dylmcd13@gmail.com>
2022-11-26 15:16:05 -0500

    Adding more text through button

--- app/src/main/java/com/example/to_dolist/MainActivity.java
+++ app/src/main/java/com/example/to_dolist/MainActivity.java
@@ -5,11 +5,14 @@ import static androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
+import android.content.Context;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.google.android.material.appbar.MaterialToolbar;
 
@@ -23,15 +26,21 @@ public class MainActivity extends AppCompatActivity {
   /** HashMap for holding tasks */
   private HashMap<String, Task> taskHashMap;
 
+  private Button addBtn;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
-    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
-    Log.i("Night Mode? ",": "+AppCompatDelegate.getDefaultNightMode());
+    LinearLayout list = findViewById(R.id.addingTest);
 
-    Button nightMode = findViewById(R.id.menu);
+    addBtn = findViewById(R.id.addTaskBtn);
+    addBtn.setOnClickListener((View v) -> {
+      TextView test = new TextView(this);
+      test.setTextColor(getResources().getColor(R.color.white));
+      test.setText("Hello@@!");
+      list.addView(test);
+    });
 
   }
 
