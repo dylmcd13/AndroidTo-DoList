ea284f7de2edcc945b1b0bf3760015a0d624cc94
dylmcd13 <dylmcd13@gmail.com>
2022-11-27 00:52:17 -0500

    WIP on placing task divider at bottom of each task

--- app/src/main/java/com/example/to_dolist/MainActivity.java
+++ app/src/main/java/com/example/to_dolist/MainActivity.java
@@ -6,13 +6,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
 import android.content.Context;
+import android.content.res.ColorStateList;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.google.android.material.appbar.MaterialToolbar;
@@ -22,7 +25,7 @@ import java.util.HashMap;
 /**
  * Main Activity Class
  * TODO: Make check box listener
- * TODO: Assign IDs to each text box
+ * TODO: Assign IDs to each text box (use View.generateViewId() probably)
  */
 public class MainActivity extends AppCompatActivity {
 
@@ -41,6 +44,9 @@ public class MainActivity extends AppCompatActivity {
 
   /** Parameters for list to have list border match parent size and have enough height to be visible */
   LinearLayout.LayoutParams listParams;
+
+
+  LinearLayout.LayoutParams checkBoxParams;
 
   /** Layout for list */
   LinearLayout list;
@@ -61,26 +67,41 @@ public class MainActivity extends AppCompatActivity {
     listParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 5);
     textParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
     textParams.leftMargin = 100;
+    checkBoxParams = new LinearLayout.LayoutParams(30,30);
     addBtn.setOnClickListener(addingTaskBox);
   }
 
   /**
-   * When button is clicked, add text and a border below the text to differentiate between tasks
+   * When button is clicked, add checkbox, text and a border below the text to differentiate between tasks
    */
   private final View.OnClickListener addingTaskBox = v -> {
-    TextView test = new TextView(this);
-    test.setTextColor(getResources().getColor(R.color.white));
+    RelativeLayout taskBox = new RelativeLayout(this);
+
+
+    CheckBox box = new CheckBox(taskBox.getContext());
+    box.setText("");
+    //box.layout(0,22,0,0);
+    //box.setPadding(10,12,10,10);
+
+    taskBox.addView(box);
+
+
+    TextView test = new TextView(taskBox.getContext());
+    //test.setTextColor(getResources().getColor(R.color.white));
     test.setTextSize(30);
     test.setText("TextView");
+
     //test.setId(); //useful for later
-    list.addView(test, textParams);
+    taskBox.addView(test, textParams);
 
 
-    View dividerBottom = new View(list.getContext());
+    View dividerBottom = new View(taskBox.getContext());
     dividerBottom.setBackgroundColor(getResources().getColor(R.color.white));
     dividerBottom.setTextAlignment(View.TEXT_ALIGNMENT_INHERIT);
 
-    list.addView(dividerBottom,listParams);
+
+    taskBox.addView(dividerBottom,listParams);
+    list.addView(taskBox);
   };
 
 }
