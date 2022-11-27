7f0fc0993eae6deb85bc914e0907fdeb33e9e1c0
dylmcd13 <dylmcd13@gmail.com>
2022-11-26 20:23:44 -0500

    Moved code in onCreate to its own initialize method

--- app/src/main/java/com/example/to_dolist/MainActivity.java
+++ app/src/main/java/com/example/to_dolist/MainActivity.java
@@ -29,37 +29,42 @@ public class MainActivity extends AppCompatActivity {
 
   private Button addBtn;
 
+  LinearLayout.LayoutParams textParams;
+
+  LinearLayout.LayoutParams listParams;
+
+  LinearLayout list;
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
-    LinearLayout list = findViewById(R.id.addingTest);
-    LinearLayout.LayoutParams listParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 5);
-
-    addBtn = findViewById(R.id.addTaskBtn);
-    addBtn.setOnClickListener((View v) -> {
-//      View dividerTop = new View(this);
-//      dividerTop.setBackgroundColor(getResources().getColor(R.color.white));
-//      list.addView(dividerTop,listParams);
-
-
-
-
-      TextView test = new TextView(this);
-      test.setTextColor(getResources().getColor(R.color.white));
-      test.setTextSize(30);
-      test.setText("TextView");
-      list.addView(test);
-
-
-      View dividerBottom = new View(list.getContext());
-      dividerBottom.setBackgroundColor(getResources().getColor(R.color.white));
-      dividerBottom.setTextAlignment(View.TEXT_ALIGNMENT_INHERIT);
-
-      list.addView(dividerBottom,listParams);
-    });
-
+    this.initialize();
   }
 
+  private void initialize(){
+    list = findViewById(R.id.addingTest);
+    addBtn = findViewById(R.id.addTaskBtn);
+    listParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 5);
+    textParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+    textParams.leftMargin = 100;
+    addBtn.setOnClickListener(addingTaskBox);
+  }
+
+  private final View.OnClickListener addingTaskBox = v -> {
+    TextView test = new TextView(this);
+    test.setTextColor(getResources().getColor(R.color.white));
+    test.setTextSize(30);
+    test.setText("TextView");
+
+    list.addView(test,textParams);
+
+
+    View dividerBottom = new View(list.getContext());
+    dividerBottom.setBackgroundColor(getResources().getColor(R.color.white));
+    dividerBottom.setTextAlignment(View.TEXT_ALIGNMENT_INHERIT);
+
+    list.addView(dividerBottom,listParams);
+  };
 
 }
