041b08c2769b9478dbb13640d46bd5a203dba9da
dylmcd13 <dylmcd13@gmail.com>
2022-11-30 22:04:43 -0500

    Automatically show textbox when adding task

--- app/src/main/java/com/example/to_dolist/MainActivity.java
+++ app/src/main/java/com/example/to_dolist/MainActivity.java
@@ -12,6 +12,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
@@ -40,14 +41,6 @@ public class MainActivity extends AppCompatActivity {
   /** Button for adding tasks */
   private Button addBtn;
 
-  /** Parameters for text so the layout wraps the content and so the text is not all the way to left */
-  RelativeLayout.LayoutParams textParams;
-
-  /** Parameters for list to have list border match parent size and have enough height to be visible */
-  RelativeLayout.LayoutParams listParams;
-
-
-  RelativeLayout.LayoutParams checkBoxParams;
 
   /** Layout for list */
   LinearLayout list;
@@ -65,10 +58,6 @@ public class MainActivity extends AppCompatActivity {
   private void initialize(){
     list = findViewById(R.id.addingTest);
     addBtn = findViewById(R.id.addTaskBtn);
-    listParams = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 10);
-    textParams = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-    textParams.leftMargin = 100;
-    checkBoxParams = new RelativeLayout.LayoutParams(30,30);
     addBtn.setOnClickListener(addingTaskBox);
   }
 
@@ -77,32 +66,8 @@ public class MainActivity extends AppCompatActivity {
    */
   private final View.OnClickListener addingTaskBox = v -> {
     RelativeLayout taskBox = (RelativeLayout) getLayoutInflater().inflate(R.layout.edit_text,null);
-
-//    CheckBox box = new CheckBox(taskBox.getContext());
-//    box.setText("");
-    //box.setBackgroundColor(getResources().getColor(R.color.white));
-    //box.layout(10,5,1,0);
-    //box.layout(0,22,0,0);
-    //box.setPadding(10,12,10,10);
-
-//    taskBox.addView(box);
-
-
-//    EditText test = new EditText(taskBox.getContext());
-//    //test.setTextColor(getResources().getColor(R.color.white));
-//    test.setTextSize(30);
-//    test.setText("TextView");
-
-    //test.setId(); //useful for later
-//    taskBox.addView(test, textParams);
-
-
-//    View dividerBottom = new View(taskBox.getContext());
-
-
-
-//    taskBox.addView(dividerBottom,listParams);
-
+    taskBox.requestFocus();
+    ((InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE)).showSoftInput(v.getRootView(),InputMethodManager.SHOW_IMPLICIT);
     list.addView(taskBox);
   };
 
